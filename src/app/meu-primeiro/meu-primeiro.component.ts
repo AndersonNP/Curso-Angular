985af5ce13b414904bfcc70f1ad9225de6482ee1
Anderson Nascimento <anderson.nascimento.info@gmail.com>
2024-07-30 09:59:55 -0300

    criando projeto no standalone

---
+++ src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'meu-primeiro-component',
+  templateUrl: './meu-primeiro.component.html',
+  styleUrl: './meu-primeiro.component.css'
+})
+export class MeuPrimeiroComponent {
+
+}
